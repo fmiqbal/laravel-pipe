21643e13ebfc796cfe7c20798066029b4e76a5f0
Fikri Muhammad Iqbal <fikri.miqbal23@gmail.com>
2019-10-07 13:12:46 +0700

    Group layout to match step group

--- resources/views/builds/show.blade.php
+++ resources/views/builds/show.blade.php
@@ -17,35 +17,57 @@
                         <i class="fas fa-check-circle float-right text-success"></i>
                     </h6>
                 </div>
+        <div class="col-md-6">
+            @foreach ($stepGroups as $group => $steps)
+                @continue((explode('-', $group)[0] ?? '') === 'pipe')
+
+                <div class="card shadow mb-4" style="border-right: 5px solid {{ $steps->every('exit_status', 0) ? '#1cc88a' : '#e74a3b' }}">
+                    <div class="card-header py-3">
+                        <h6 class="m-0 font-weight-bold text-primary">{{ $group }}</h6>
+                    </div>
                 <div class="card-body">
                     <ul class="list-group">
-                        @foreach ($build->meta_steps as $step)
-                            <li class="list-group-item">{{ $step[0] }}
-                                @if ($step[1] == 0)
-                                    <i class="fa fa-check-circle float-right text-success"></i>
+                            @foreach ($steps as $step)
+                                <li class="list-group-item">{{ $step->command }}
+                                    @if ($step->exit_status === null)
+                                        <i class="fas fa-spin fa-circle-notch float-right text-muted"></i>
+                                    @elseif ($step->exit_status === 0)
+                                        <i class="fas fa-check-circle float-right text-success"></i>
                                 @else
-                                    <i class="fa fa-times-circle float-right text-danger"></i>
+                                        <i class="fas fa-times-circle float-right text-danger"></i>
                                 @endif
                             </li>
                         @endforeach
                     </ul>
                 </div>
             </div>
+
+            @endforeach
         </div>
         <div class="col-md-6">
             <div class="card shadow mb-4">
                 <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">Terminal Output                    </h6>
+                    <h6 class="m-0 font-weight-bold text-primary">Live Output </h6>
                 </div>
                 <div class="card-body">
                     <span id="terminal-output">
-                        @foreach ($build->meta['lines'] ?? [] as $line)
-                            {{ $line }} <br>
+                        <div {{ $iterate = 1 }}></div>
+                        @foreach ($build->steps as $step)
+                            @continue(empty($step->output))
+                            @continue((explode('-', $step->group)[0] ?? '') === 'pipe')
+
+                            Executing : {{ $step->command }}
+                            <br>
+                            {{ $iterate++ }} | {{ $step->output }}
+                            <br>
+                            <br>
                         @endforeach
                     </span>
 
                     @if (! in_array($build->status, \Fikrimi\Pipe\Models\Build::getFinishStatuses()))
                         <i id="output-spinner" class="fas fa-lg fa-circle-notch fa-spin m-t-20"></i>
+                    @else
+                        Deploy has been completed with status: {{ $build->status_name }}
                     @endif
                 </div>
             </div>
