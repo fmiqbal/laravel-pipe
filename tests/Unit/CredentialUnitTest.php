63bcd3de90b26b6cc3e677a4c53cb91108ff2be5
Fikri Muhammad Iqbal <fikri.miqbal23@gmail.com>
2019-10-21 12:46:32 +0700

    add docs

--- tests/Unit/CredentialUnitTest.php
+++ tests/Unit/CredentialUnitTest.php
@@ -28,6 +28,9 @@ class CredentialUnitTest extends TestCase
     {
         $credData = factory(Credential::class)->create();
 
+        /**
+         * @url https://stackoverflow.com/questions/19989481/how-to-determine-if-a-string-is-a-valid-v4-uuid
+         */
         $UUIDv4 = '/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i';
 
         $this->assertRegExp($UUIDv4, $credData->fingerprint);
